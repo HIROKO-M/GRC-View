89746f9f69076a4598d8f58ccb6f2c8eb8f3845d
EC2 Default User <ec2-user@ip-172-31-18-25.ec2.internal>
2019-02-06 04:16:50 +0000

    grc-view init

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                
+                <h1>検索順位チェックツール　GRC</h1>
+            </div>
+            
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">新規登録</a></li>
+                    <li><a href="#">ログイン</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>GRC CheckTool</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <!--datepicker plugin -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+
+    </head>
+    
+    <body>
+        @include('commons.navbar')
+        
+        @yield('cover')
+        
+        <div class="container">
+            @include('commons.error_messages')
+            @yield('content')
+        </div>
+    </body>
+    
+</html>
