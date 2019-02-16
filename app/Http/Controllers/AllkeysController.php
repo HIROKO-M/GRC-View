da8f403b341dbcb702d5fa72a431cd093350a800
EC2 Default User <ec2-user@ip-172-31-18-25.ec2.internal>
2019-02-16 20:12:30 +0000

    delete orderBy for pgsql

--- app/Http/Controllers/AllkeysController.php
+++ app/Http/Controllers/AllkeysController.php
@@ -32,45 +32,52 @@ class AllkeysController extends Controller
  
         // ランキング表示のためのキーワード選択で利用
 
-        $checkeddays = Gdata::orderBy('created_at', 'asc')->whereIn('grc_keyword', $selkeys)->groupBy('check_date')->get();
-        $yranks = Gdata::orderBy('created_at', 'asc')->whereIn('grc_keyword', $selkeys)->get();
-        // $yranks = Gdata::orderBy('created_at', 'desc')->whereIn('grc_keyword', $selkeys)->get()->map(function ($item, $key) {
+        //$checkeddays = Gdata::orderBy('created_at', 'asc')->whereIn('grc_keyword', $selkeys)->groupBy('check_date')->get();
+        $ranks = Gdata::orderBy('created_at', 'asc')->whereIn('grc_keyword', $selkeys)->get();
+        // $ranks = Gdata::orderBy('created_at', 'desc')->whereIn('grc_keyword', $selkeys)->get()->map(function ($item, $key) {
         //     return $item->y_rank;
         // })->all();
         
-        // error_log(var_dump($yranks->toArray()));
+        // error_log(var_dump($ranks->toArray()));
 
         
         
         // これでとりあえず配列に出来る。（スマートとは言えないが）
+        // $huga = array();
+        // foreach($ranks as $day) {
+        //    array_push($huga, $day->check_date);
+        // }
+        
+        
         $huga = array();
-        foreach($checkeddays as $day) {
-            array_push($huga, $day->check_date);
-        }
-        
-        
         $g_array = array();
         $y_array = array();
-        foreach($yranks as $yrank) {
-            array_push($y_array, $yrank->y_rank);
-            array_push($g_array, $yrank->g_rank);
+        foreach($ranks as $rank) {
+            array_push($huga, $rank->check_date);
+            array_push($y_array, $rank->y_rank);
+            array_push($g_array, $rank->g_rank);
         }
         
-        $yranks = array_map(function($value){ return (int)$value; }, $y_array);
-        $granks = array_map(function($value){ return (int)$value; }, $g_array);
+        $rankday = array();
+        $rankday = array_unique($huga);             // check_date の重複を削除
         
-        $yranks_rep = str_replace('0', '100', $yranks);
-        $granks_rep = str_replace('0', '100', $granks);
+        $ranks = array_map(function($value){ return (int)$value; }, $y_array);  //int型に型変更
+        $granks = array_map(function($value){ return (int)$value; }, $g_array); //int型に型変更
+        
+        $ranks_rep = str_replace('0', '100', $ranks);       // Chart表示のため、「0」→「100」へ置き換え
+        $granks_rep = str_replace('0', '100', $granks);     // Chart表示のため、「0」→「100」へ置き換え
 
         // error_log(var_dump($y_array));        
-        // error_log(var_dump($yranks));
+        // error_log(var_dump($ranks));
+        // error_log(var_dump($huga));
+        // error_log(var_dump($rankday));
 
         return view('allkeys.index', [
             'orders' => $orders,
             'date' => $date,
-            'yranks_rep' => $yranks_rep,
+            'yranks_rep' => $ranks_rep,
             'granks_rep' => $granks_rep,
-            'checkeddays' => $huga,
+            'checkeddays' => $rankday,
             'selkey' => $selkey,
         ]);
         
