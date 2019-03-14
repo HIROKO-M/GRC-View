9dc4de10534484e1d6017545d4bbafb12994b806
EC2 Default User <ec2-user@ip-172-31-18-25.ec2.internal>
2019-03-14 09:42:37 +0000

    migration file change

--- database/migrations/2019_03_14_183459_change_impdatas_table.php
+++ database/migrations/2019_03_14_183459_change_impdatas_table.php
@@ -13,8 +13,13 @@ class ChangeImpdatasTable extends Migration
     public function up()
     {
         Schema::table('impdatas', function (Blueprint $table) {
+            $table->string('y_rank')->nullable()->change();
+            $table->string('g_rank')->nullable()->change();
+            $table->date('check_date')->change();
             $table->integer('y_count')->nullable()->change();
             $table->integer('g_count')->nullable()->change();
+
+
         });
     }
 
@@ -26,8 +31,12 @@ class ChangeImpdatasTable extends Migration
     public function down()
     {
         Schema::table('impdatas', function (Blueprint $table) {
+            $table->dropColumn('y_rank');
+            $table->dropColumn('g_rank');
+            $table->dropColumn('check_date');
             $table->dropColumn('y_count');
             $table->dropColumn('g_count');
+
         });
     }
 }
