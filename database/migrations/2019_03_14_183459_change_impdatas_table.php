20beaf029fee66648aed9412c13141f8dc8247b9
EC2 Default User <ec2-user@ip-172-31-18-25.ec2.internal>
2019-03-15 07:30:52 +0000

    debug 6

--- database/migrations/2019_03_14_183459_change_impdatas_table.php
+++ database/migrations/2019_03_14_183459_change_impdatas_table.php
@@ -14,7 +14,7 @@ class ChangeImpdatasTable extends Migration
     {
         Schema::table('impdatas', function (Blueprint $table) {
             $table->string('y_rank')->nullable()->change();
-            $table->string('g_rank')->nullable()->change();
+            //$table->string('g_rank')->nullable()->change();
             //$table->date('check_date')->change();
             $table->integer('y_count')->nullable()->change();
             $table->integer('g_count')->nullable()->change();
@@ -32,7 +32,7 @@ class ChangeImpdatasTable extends Migration
     {
         Schema::table('impdatas', function (Blueprint $table) {
             $table->dropColumn('y_rank');
-            $table->dropColumn('g_rank');
+            //$table->dropColumn('g_rank');
             $table->dropColumn('check_date');
             $table->dropColumn('y_count');
             $table->dropColumn('g_count');
