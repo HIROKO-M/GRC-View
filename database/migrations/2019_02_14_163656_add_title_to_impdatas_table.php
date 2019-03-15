502b03609bf40fb98e05cce36cbf2e101869829d
EC2 Default User <ec2-user@ip-172-31-18-25.ec2.internal>
2019-03-15 08:20:04 +0000

    debug 8

--- database/migrations/2019_02_14_163656_add_title_to_impdatas_table.php
+++ database/migrations/2019_02_14_163656_add_title_to_impdatas_table.php
@@ -35,3 +35,14 @@ class AddTitleToImpdatasTable extends Migration
         });
     }
 }
+
+/**
+ * The development database settings. These get merged with the global settings.
+ */
+
+return array(
+ 'default' => array(
+  'charset'	=> NULL,
+  'identifier' => "\""      /* for PostgreSQL */
+  ),
+);
